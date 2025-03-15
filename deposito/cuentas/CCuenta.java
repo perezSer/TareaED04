97cd8d6e423c63bd16f2551b3ae144c131be2fe2
Sergio P <ue355579@edu.xunta.gal>
2025-03-15 20:56:37 +0100

    Tarea ED04 perez_canay_sergio

--- deposito/cuentas/CCuenta.java
+++ deposito/cuentas/CCuenta.java
@@ -7,21 +7,35 @@ public class CCuenta {
     private double saldo;
     private double tipoInterés;
 
-    public CCuenta()
-    {
+    public CCuenta() {
     }
 
+    /**
+     * @param nom El nombre del titular de la cuenta.
+     * @param cue Número de cuenta.
+     * @param sal Saldo inicial de la cuenta.
+     * @param tipo Tipo de interés de la cuenta.
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         setNombre(nom);
         setCuenta(cue);
         setSaldo(sal);
-    }
-
+    }	
+    
+    /* @return El saldo actual de la cuenta
+     * 
+     */
     public double estado()
     {
         return getSaldo();
     }
+    
+    /**Ingresa una cantidad en la cuenta
+    *
+    *@param cantidad Es la cantidad a ingresar en la cuenta.
+    *@throws Exception si la cantidad es negativa
+    */
 
     public void ingresar(double cantidad) throws Exception
     {
@@ -29,7 +43,13 @@ public class CCuenta {
             throw new Exception("No se puede ingresar una cantidad negativa");
         setSaldo(getSaldo() + cantidad);
     }
+    
 
+    /**Retira una cantidad de la cuenta
+     * 
+     * @ param cantidad a retirar de la cuenta
+     * @throws Exception Si la cantidad es negativa o saldo insuficiente lanza la excepción
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
@@ -39,34 +59,62 @@ public class CCuenta {
         setSaldo(getSaldo() - cantidad);
     }
 
+    /** 
+     * 
+     * @return Obitiene el tipo de interés de la cuenta
+     */
+    
 	private double getTipoInterés() {
 		return tipoInterés;
 	}
 
+	/**
+	 * @param tipoInterés es el tipo de interés que se desea establecer
+	 */
 	private void setTipoInterés(double tipoInterés) {
 		this.tipoInterés = tipoInterés;
 	}
 
+	
+	/** @return Saldo de la cuenta
+	 * 
+	 */
 	private double getSaldo() {
 		return saldo;
 	}
 
+	/**@param saldo El saldo que se establece en la cuenta
+	 * 
+	 */
 	private void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
 
+	/** @return el número de cuenta
+	 * 
+	 */
 	private String getCuenta() {
 		return cuenta;
 	}
 
+	/** @param cuenta El número de cuenta que se establece
+	 * 
+	 */
 	private void setCuenta(String cuenta) {
 		this.cuenta = cuenta;
 	}
+	
 
+	/** @return El nombre del titular de la cuenta
+	 * 
+	 */
 	private String getNombre() {
 		return nombre;
 	}
 
+	/** @param nombre El nombre del titular que se establece
+	 * 
+	 */
 	private void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
--- deposito/cuentas/Main.java
+++ deposito/cuentas/Main.java
@@ -1,6 +1,6 @@
 package cuentas;
 public class Main {
-
+								
     public static void main(String[] args) {
         CCuenta cuenta1;
        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
